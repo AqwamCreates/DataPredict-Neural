f29019ed9e46ebd3fe3caa65487be361ce4bdcbe
Aqwam Harish Aiman <aqwam.harish.aiman@gmail.com>
2025-07-24 16:15:44 +0800

    Updates

---
+++ src/OperatorBlocks/Power.lua
@@ -0,0 +1,89 @@
+--[[
+
+	--------------------------------------------------------------------
+
+	Aqwam's Deep Learning Library (DataPredict Neural)
+
+	Author: Aqwam Harish Aiman
+	
+	Email: aqwam.harish.aiman@gmail.com
+	
+	YouTube: https://www.youtube.com/channel/UCUrwoxv5dufEmbGsxyEUPZw
+	
+	LinkedIn: https://www.linkedin.com/in/aqwam-harish-aiman/
+	
+	--------------------------------------------------------------------
+		
+	By using this library, you agree to comply with our Terms and Conditions in the link below:
+	
+	https://github.com/AqwamCreates/DataPredict-Neural/blob/main/docs/TermsAndConditions.md
+	
+	--------------------------------------------------------------------
+	
+	DO NOT REMOVE THIS TEXT!
+	
+	--------------------------------------------------------------------
+
+--]]
+
+local BaseOperatorBlock = require(script.Parent.BaseOperatorBlock)
+
+local AqwamTensorLibrary = require(script.Parent.Parent.AqwamTensorLibraryLinker.Value)
+
+PowerBlock = {}
+
+PowerBlock.__index = PowerBlock
+
+setmetatable(PowerBlock, BaseOperatorBlock)
+
+function PowerBlock.new()
+
+	local NewPowerBlock = BaseOperatorBlock.new()
+
+	setmetatable(NewPowerBlock, PowerBlock)
+
+	NewPowerBlock:setName("Power")
+	
+	NewPowerBlock:setFirstDerivativeFunctionRequiresTransformedTensor(false)
+	
+	NewPowerBlock:setFunction(function(inputTensorArray)
+		
+		return AqwamTensorLibrary:power(table.unpack(inputTensorArray))
+	
+	end)
+
+	NewPowerBlock:setChainRuleFirstDerivativeFunction(function(initialPartialFirstDerivativeTensor, transformedTensor, inputTensorArray)
+		
+		local chainRuleFirstDerivativeTensorArray = {}
+		
+		local inputTensor1 = inputTensorArray[1]
+
+		local inputTensor2 = inputTensorArray[2]
+
+		local dimensionSizeArray1 = AqwamTensorLibrary:getDimensionSizeArray(inputTensor1)
+		
+		local dimensionSizeArray2 = AqwamTensorLibrary:getDimensionSizeArray(inputTensor2)
+		
+		local exponentMinusOneTensor = AqwamTensorLibrary:subtract(inputTensor2, 1)
+		
+		local chainRuleFirstDerivativeTensor1Part1 = AqwamTensorLibrary:power(inputTensor1, exponentMinusOneTensor)
+		
+		local chainRuleFirstDerivativeTensor1 = AqwamTensorLibrary:multiply(initialPartialFirstDerivativeTensor, inputTensor1, chainRuleFirstDerivativeTensor1Part1)
+		
+		chainRuleFirstDerivativeTensorArray[1] = NewPowerBlock:collapseTensor(chainRuleFirstDerivativeTensor1, dimensionSizeArray1)
+		
+		local partialChainRuleFirstDerivativeTensor2 = AqwamTensorLibrary:applyFunction(function(base, exponent) return (math.pow(base, exponent) * math.log(base)) end, inputTensor1, inputTensor2)
+		
+		local chainRuleFirstDerivativeTensor2 = AqwamTensorLibrary:multiply(initialPartialFirstDerivativeTensor, partialChainRuleFirstDerivativeTensor2)
+		
+		chainRuleFirstDerivativeTensorArray[2] = NewPowerBlock:collapseTensor(chainRuleFirstDerivativeTensor2, dimensionSizeArray2)
+
+		return chainRuleFirstDerivativeTensorArray
+		
+	end)
+
+	return NewPowerBlock
+
+end
+
+return PowerBlock
---
+++ src/OperatorBlocks/StandardDeviation.lua
@@ -0,0 +1,91 @@
+--[[
+
+	--------------------------------------------------------------------
+
+	Aqwam's Deep Learning Library (DataPredict Neural)
+
+	Author: Aqwam Harish Aiman
+	
+	Email: aqwam.harish.aiman@gmail.com
+	
+	YouTube: https://www.youtube.com/channel/UCUrwoxv5dufEmbGsxyEUPZw
+	
+	LinkedIn: https://www.linkedin.com/in/aqwam-harish-aiman/
+	
+	--------------------------------------------------------------------
+		
+	By using this library, you agree to comply with our Terms and Conditions in the link below:
+	
+	https://github.com/AqwamCreates/DataPredict-Neural/blob/main/docs/TermsAndConditions.md
+	
+	--------------------------------------------------------------------
+	
+	DO NOT REMOVE THIS TEXT!
+	
+	--------------------------------------------------------------------
+
+--]]
+
+local BaseOperatorBlock = require(script.Parent.BaseOperatorBlock)
+
+local AqwamTensorLibrary = require(script.Parent.Parent.AqwamTensorLibraryLinker.Value)
+
+StandardDeviationBlock = {}
+
+StandardDeviationBlock.__index = StandardDeviationBlock
+
+setmetatable(StandardDeviationBlock, BaseOperatorBlock)
+
+function StandardDeviationBlock.new(parameterDictionary)
+
+	local NewStandardDeviationBlock = BaseOperatorBlock.new()
+
+	setmetatable(NewStandardDeviationBlock, StandardDeviationBlock)
+
+	NewStandardDeviationBlock:setName("StandardDeviation")
+
+	NewStandardDeviationBlock:setFirstDerivativeFunctionRequiresTransformedTensor(false)
+
+	local dimension = parameterDictionary.dimension
+
+	if (not dimension) then error("No dimension is selected.") end
+
+	NewStandardDeviationBlock.dimension = dimension
+
+	NewStandardDeviationBlock:setFunction(function(inputTensorArray)
+
+		return AqwamTensorLibrary:standardDeviation(inputTensorArray[1], NewStandardDeviationBlock.dimension)
+
+	end)
+
+	NewStandardDeviationBlock:setChainRuleFirstDerivativeFunction(function(initialPartialFirstDerivativeTensor, transformedTensor, inputTensorArray)
+
+		local inputTensor = inputTensorArray[1]
+		
+		local dimension = NewStandardDeviationBlock.dimension
+
+		local dimensionSize = AqwamTensorLibrary:getDimensionSizeArray(inputTensor)[dimension]
+
+		local standardDeviationTensor = AqwamTensorLibrary:standardDeviation(inputTensor, dimension)
+
+		local chainRuleFirstDerivativeTensorPart1 = AqwamTensorLibrary:multiply(2, standardDeviationTensor, dimensionSize)
+
+		local chainRuleFirstDerivativeTensor = AqwamTensorLibrary:divide(initialPartialFirstDerivativeTensor, chainRuleFirstDerivativeTensorPart1)
+
+		return {chainRuleFirstDerivativeTensor}
+
+	end)
+
+	return NewStandardDeviationBlock
+
+end
+
+function StandardDeviationBlock:setParameters(parameterDictionary)
+
+	parameterDictionary = parameterDictionary or {}
+
+	self.dimension = parameterDictionary.dimension or self.dimension
+
+end
+
+return StandardDeviationBlock
