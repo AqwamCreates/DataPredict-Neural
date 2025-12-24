5f22575665f61eba6f96d9a78512a9fa8fb7b660
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-12-25 07:54:23 +0800

    Update Maximum.lua

--- src/OperatorBlocks/Maximum.lua
+++ src/OperatorBlocks/Maximum.lua
@@ -30,7 +30,7 @@ local BaseOperatorBlock = require(script.Parent.BaseOperatorBlock)
 
 local AqwamTensorLibrary = require(script.Parent.Parent.AqwamTensorLibraryLinker.Value)
 
-MaximumBlock = {}
+local MaximumBlock = {}
 
 MaximumBlock.__index = MaximumBlock
 
