ffc157743bfdfb0b9664f5e28ba76f37ca688d66
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-12-25 07:53:02 +0800

    Update PairwiseDistance.lua

--- src/OperatorBlocks/PairwiseDistance.lua
+++ src/OperatorBlocks/PairwiseDistance.lua
@@ -30,7 +30,7 @@ local BaseOperatorBlock = require(script.Parent.BaseOperatorBlock)
 
 local AqwamTensorLibrary = require(script.Parent.Parent.AqwamTensorLibraryLinker.Value)
 
-PairwiseDistanceBlock = {}
+local PairwiseDistanceBlock = {}
 
 PairwiseDistanceBlock.__index = PairwiseDistanceBlock
 
