f937616cc63d9f228ff232d5a727b3c43450c626
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-12-25 06:11:18 +0800

    Update Softmax.lua

--- src/ActivationBlocks/Softmax.lua
+++ src/ActivationBlocks/Softmax.lua
@@ -30,7 +30,7 @@ local BaseActivationBlock = require(script.Parent.BaseActivationBlock)
 
 local AqwamTensorLibrary = require(script.Parent.Parent.AqwamTensorLibraryLinker.Value)
 
-SoftmaxBlock = {}
+local SoftmaxBlock = {}
 
 SoftmaxBlock.__index = SoftmaxBlock
 
