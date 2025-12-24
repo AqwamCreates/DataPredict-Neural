20a896d1768ecf3714d5f7f68756df7ffd2be10b
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-12-25 07:53:45 +0800

    Update Mean.lua

--- src/OperatorBlocks/Mean.lua
+++ src/OperatorBlocks/Mean.lua
@@ -30,7 +30,7 @@ local BaseOperatorBlock = require(script.Parent.BaseOperatorBlock)
 
 local AqwamTensorLibrary = require(script.Parent.Parent.AqwamTensorLibraryLinker.Value)
 
-MeanBlock = {}
+local MeanBlock = {}
 
 MeanBlock.__index = MeanBlock
 
