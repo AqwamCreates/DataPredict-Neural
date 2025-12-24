1337db43ad75edc951df5d80488bf7fb543fa862
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-12-25 06:36:29 +0800

    Update BaseExpansionBlocks.lua

--- src/ExpansionBlocks/BaseExpansionBlocks.lua
+++ src/ExpansionBlocks/BaseExpansionBlocks.lua
@@ -30,7 +30,7 @@ local AqwamTensorLibrary = require(script.Parent.Parent.AqwamTensorLibraryLinker
 
 local BaseFunctionBlock = require(script.Parent.Parent.Cores.BaseFunctionBlock)
 
-BaseExpansionBlock = {}
+local BaseExpansionBlock = {}
 
 BaseExpansionBlock.__index = BaseExpansionBlock
 
