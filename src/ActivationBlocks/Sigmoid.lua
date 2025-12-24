ddd4977590970d300c1ad225242e66d0e56a9db4
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-12-25 06:11:45 +0800

    Update Sigmoid.lua

--- src/ActivationBlocks/Sigmoid.lua
+++ src/ActivationBlocks/Sigmoid.lua
@@ -30,7 +30,7 @@ local BaseActivationBlock = require(script.Parent.BaseActivationBlock)
 
 local AqwamTensorLibrary = require(script.Parent.Parent.AqwamTensorLibraryLinker.Value)
 
-SigmoidBlock = {}
+local SigmoidBlock = {}
 
 SigmoidBlock.__index = SigmoidBlock
 
