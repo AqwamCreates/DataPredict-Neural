a83f43154f8613b9b7ac3d2184f862b5eacc0c50
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-12-25 07:55:01 +0800

    Update Exponent.lua

--- src/OperatorBlocks/Exponent.lua
+++ src/OperatorBlocks/Exponent.lua
@@ -30,7 +30,7 @@ local BaseOperatorBlock = require(script.Parent.BaseOperatorBlock)
 
 local AqwamTensorLibrary = require(script.Parent.Parent.AqwamTensorLibraryLinker.Value)
 
-ExponentBlock = {}
+local ExponentBlock = {}
 
 ExponentBlock.__index = ExponentBlock
 
