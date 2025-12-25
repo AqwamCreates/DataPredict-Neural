7786ba3f8662f5060ec9b369664bdfafd67b87cb
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-12-25 08:09:19 +0800

    Update Clamp.lua

--- src/OperatorBlocks/Clamp.lua
+++ src/OperatorBlocks/Clamp.lua
@@ -30,7 +30,7 @@ local BaseOperatorBlock = require(script.Parent.BaseOperatorBlock)
 
 local AqwamTensorLibrary = require(script.Parent.Parent.AqwamTensorLibraryLinker.Value)
 
-ClampBlock = {}
+local ClampBlock = {}
 
 ClampBlock.__index = ClampBlock
 
