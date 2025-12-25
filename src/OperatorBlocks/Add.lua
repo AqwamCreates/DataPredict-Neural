e4253bcb80bc657a6d157ca3d061476d420abc9b
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2025-12-25 08:08:50 +0800

    Update Add.lua

--- src/OperatorBlocks/Add.lua
+++ src/OperatorBlocks/Add.lua
@@ -30,7 +30,7 @@ local BaseOperatorBlock = require(script.Parent.BaseOperatorBlock)
 
 local AqwamTensorLibrary = require(script.Parent.Parent.AqwamTensorLibraryLinker.Value)
 
-AddBlock = {}
+local AddBlock = {}
 
 AddBlock.__index = AddBlock
 
