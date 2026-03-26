a43b297321d19710af792cf3cf7f5cb242f24841
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2026-03-26 18:17:33 +0800

    Update RecurrentDeepDoubleQLearningV2.lua

--- src/RecurrentModels/RecurrentDeepDoubleQLearningV2.lua
+++ src/RecurrentModels/RecurrentDeepDoubleQLearningV2.lua
@@ -142,9 +142,9 @@ function RecurrentDoubleQLearningModel.new(parameterDictionary)
 
 		Model:setWeightTensorArray(PrimaryWeightTensorArray, true)
 
-		Model:forwardPropagate(previousFeatureTensor, true)
+		Model:forwardPropagate(previousFeatureTensor, primaryHiddenStateTensor)
 
-		Model:update(negatedTemporalDifferenceErrorTensor, true)
+		Model:update(negatedTemporalDifferenceErrorTensor)
 
 		PrimaryWeightTensorArray = Model:getWeightTensorArray(true)
 
